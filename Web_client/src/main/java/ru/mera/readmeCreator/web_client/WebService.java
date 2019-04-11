9f13d789570d73e684cf42407a84381709964ece
syakim <syakim@mera.ru>
2019-04-11 14:28:49 +0300

    Web_client: added comments to WebService

--- Web_client/src/main/java/ru/mera/readmeCreator/web_client/WebService.java
+++ Web_client/src/main/java/ru/mera/readmeCreator/web_client/WebService.java
@@ -3,13 +3,37 @@ package ru.mera.readmeCreator.web_client;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * Interface for web services in this program.
+ * For now, classes which extends this abstract class,
+ * must define when they are available (isAvailable() method)
+ * and must be able to sendGetRequests (sendGetRequest() method)
+ */
 public abstract class WebService {
+    //URL of web service
     protected URL webServiceURL;
+
+    protected URL getWebServiceURL() {
+        return this.webServiceURL;
+    }
 
     protected void setWebServiceURL(String webServiceURL) throws MalformedURLException {
         this.webServiceURL = new URL(webServiceURL);
     }
 
-    public abstract boolean isAvailable();
+    /**
+     * Defines when this web service is available. For this, sending 'GET' request
+     * directly to service URL
+     * @return true - service is available and false otherwise
+     */
+    public boolean isAvailable() {
+        return sendGetRequest("") != -1;
+    }
+
+    /**
+     * Method for sending 'GET' requests to web service
+     * @param getMapping mapping for 'GET' request
+     * @return response code or -1 if connection to server was refused
+     */
     public abstract int sendGetRequest(String getMapping);
 }
