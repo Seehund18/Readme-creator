8526f006eb6d46218f2ce8df9874ae90dc76a87f
syakim <syakim@mera.ru>
2019-04-15 19:31:55 +0300

    Web_service: added UserData and post request processing

--- Web_service/src/main/java/ru/mera/readmeCreator/server/FileController.java
+++ Web_service/src/main/java/ru/mera/readmeCreator/server/FileController.java
@@ -5,9 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -44,6 +42,36 @@ public class FileController {
             throw new GeneratorException(ex.getMessage(), ex);
         }
         log.info("Document {} was generated. Sending to client", name);
+
+        //Setting Http response headers
+        HttpHeaders responseHeader = new HttpHeaders();
+        responseHeader.set(HttpHeaders.CONTENT_TYPE, "application/rtf; charset=utf-8");
+        responseHeader.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + document.getName());
+        responseHeader.set(HttpHeaders.CONTENT_LENGTH, String.valueOf(document.length()));
+
+        return new ResponseEntity<>(new FileSystemResource(document), responseHeader, HttpStatus.OK);
+    }
+
+    /**
+     * Handler method for file download requests
+//     * @param name file name
+     * @return http response with file
+     */
+    @PostMapping("/files/Readme.rtf")
+    public ResponseEntity<FileSystemResource> sendDocument(@RequestBody UserData userData) {
+
+        log.info("Received 'POST' request to generate Readme file");
+        log.info(userData.getInfo());
+        File document;
+        try {
+            //Trying to generate file with given name
+            document = fileGenerator.generate("Readme.rtf");
+        } catch (IOException ex) {
+            //If some problems occurred, GeneratorException is thrown
+            //which will be caught and handled by GeneratorExceptionAdvice
+            throw new GeneratorException(ex.getMessage(), ex);
+        }
+        log.info("Document Readme.rtf was generated. Sending to client");
 
         //Setting Http response headers
         HttpHeaders responseHeader = new HttpHeaders();
---
+++ Web_service/src/main/java/ru/mera/readmeCreator/server/UserData.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Avaya Inc., All Rights Reserved. THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF Avaya Inc. The copyright
+ * notice above does not evidence any actual or intended publication of such source code. Some third-party source code
+ * components may have been modified from their original versions by Avaya Inc. The modifications are Copyright Avaya
+ * Inc., All Rights Reserved. Avaya - Confidential & Restricted. May not be distributed further without written
+ * permission of the Avaya owner.
+ */
+
+package ru.mera.readmeCreator.server;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+
+public class UserData {
+
+    private String info;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    @JsonCreator
+    public UserData(String info) {
+        this.info = info;
+    }
+}
