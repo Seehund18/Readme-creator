059d400c56a571c95d9b3edd27214f210a6fdc77
syakim <syakim@mera.ru>
2019-05-16 14:54:26 +0300

    Change deploy scripts

--- Desktop_client/deploy.bat
+++ Desktop_client/deploy.bat
@@ -1,5 +1,6 @@
 mkdir deployment
 call mvn clean package
 xcopy "target\*jar-with-dependencies.jar" "deployment"
-echo java -jar Desktop_client-0.1.0-jar-with-dependencies.jar > deployment\start.bat
+echo java -jar Desktop_client-1.0-jar-with-dependencies.jar > deployment\start.bat
+echo pause >> deployment\start.bat
 pause
--- Web_service/deploy.bat
+++ Web_service/deploy.bat
@@ -1,5 +1,6 @@
 mkdir deployment
 call mvn clean package
 xcopy "target\*.jar" "deployment"
-echo java -jar Web_service-0.1.0.jar > deployment\start.bat
+echo java -jar Web_service-1.0.jar > deployment\start.bat
+echo pause >> deployment\start.bat
 pause
