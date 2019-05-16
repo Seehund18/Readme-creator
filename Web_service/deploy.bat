4292424682ce8e9fe338a5e9c27fb675e965b300
syakim <syakim@mera.ru>
2019-05-16 15:48:02 +0300

    Web_service: edit deploy.bat so it creates script which seeks for application.properties in conf directory

--- Web_service/deploy.bat
+++ Web_service/deploy.bat
@@ -1,6 +1,7 @@
 mkdir deployment
 call mvn clean package
 xcopy "target\*.jar" "deployment"
-echo java -jar Web_service-1.0.jar > deployment\start.bat
+move conf deployment
+echo java -jar Web_service-1.0.jar --spring.config.location=conf/application.properties > deployment\start.bat
 echo pause >> deployment\start.bat
 pause
